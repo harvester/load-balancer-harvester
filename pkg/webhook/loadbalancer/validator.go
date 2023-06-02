8206ea5e573b531b4ef95db7aa105f383ffbc8cb
Canwu Yao <yaocanwu@gmail.com>
2023-06-02 12:33:40 +0800

    Validate load balancer listeners

--- pkg/webhook/loadbalancer/validator.go
+++ pkg/webhook/loadbalancer/validator.go
@@ -28,6 +28,10 @@ func NewValidator(vmiCache ctlkubevirtv1.VirtualMachineInstanceCache) admission.
 func (v *validator) Create(_ *admission.Request, newObj runtime.Object) error {
 	lb := newObj.(*lbv1.LoadBalancer)
 
+	if err := checkListeners(lb); err != nil {
+		return fmt.Errorf("create loadbalancer %s/%s failed: %w", lb.Namespace, lb.Name, err)
+	}
+
 	ok, err := v.matchAtLeastOneVmi(lb)
 	if err != nil {
 		return fmt.Errorf("create loadbalancer %s/%s failed: %w", lb.Namespace, lb.Name, err)
@@ -44,6 +48,10 @@ func (v *validator) Update(_ *admission.Request, oldObj, newObj runtime.Object) 
 
 	if lb.DeletionTimestamp != nil {
 		return nil
+	}
+
+	if err := checkListeners(lb); err != nil {
+		return fmt.Errorf("update loadbalancer %s/%s failed: %w", lb.Namespace, lb.Name, err)
 	}
 
 	ok, err := v.matchAtLeastOneVmi(lb)
@@ -83,4 +91,31 @@ func (v *validator) matchAtLeastOneVmi(lb *lbv1.LoadBalancer) (bool, error) {
 	}
 
 	return len(vmis) > 0, nil
+}
+
+func checkListeners(lb *lbv1.LoadBalancer) error {
+	nameMap, portMap, backendMap := map[string]bool{}, map[int32]int{}, map[int32]int{}
+	for i, listener := range lb.Spec.Listeners {
+		// check listener name
+		if _, ok := nameMap[listener.Name]; ok {
+			return fmt.Errorf("listener has duplicate name %s", listener.Name)
+		}
+		nameMap[listener.Name] = true
+
+		// check listener port
+		if index, ok := portMap[listener.Port]; ok {
+			return fmt.Errorf("listener %s has duplicate port %d with listener %s", listener.Name,
+				listener.Port, lb.Spec.Listeners[index].Name)
+		}
+		portMap[listener.Port] = i
+
+		// check backend port
+		if index, ok := backendMap[listener.BackendPort]; ok {
+			return fmt.Errorf("listener %s has duplicate backend port %d with listener %s", listener.Name,
+				listener.BackendPort, lb.Spec.Listeners[index].Name)
+		}
+		backendMap[listener.BackendPort] = i
+	}
+
+	return nil
 }
