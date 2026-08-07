b149927a3ece9e23ae2aabdc32aad7d73d3019f5
Jian Wang <jian.wang@suse.com>
2026-08-07 08:35:32 +0200

    Revert "Enhance global ippool selection with network matching"
    This reverts commit 5499454a8f09d50505778e3da23a9fa9150cf3b6.
    
    Related PR https://github.com/harvester/load-balancer-harvester/pull/104
    
    Signed-off-by: Jian Wang <jian.wang@suse.com>
    (cherry picked from commit e861dc948324084793b8af7dee22451d8a0d619c)

--- pkg/webhook/ippool/mutator.go
+++ pkg/webhook/ippool/mutator.go
@@ -85,14 +85,12 @@ func (i *ipPoolMutator) getLabelPatch(pool *lbv1.IPPool) (admission.Patch, error
 	}
 
 	vidStr := strconv.Itoa(vid)
-	isGlobalStr := strconv.FormatBool(ipam.IsGlobalIPPool(pool))
+	isGlobalStr := strconv.FormatBool(isGlobalIPPool(pool))
 	if labels[utils.KeyVid] == vidStr && labels[utils.KeyGlobalIPPool] == isGlobalStr {
 		return patch, nil
 	}
 
 	labels[utils.KeyVid] = vidStr
-	// Label the pool with its global status
-	// Each network can have at most one global IPPool, which is ensured by the validator
 	labels[utils.KeyGlobalIPPool] = isGlobalStr
 
 	return append(patch, admission.PatchOp{
@@ -100,4 +98,9 @@ func (i *ipPoolMutator) getLabelPatch(pool *lbv1.IPPool) (admission.Patch, error
 		Path:  "/metadata/labels",
 		Value: labels,
 	}), nil
+}
+
+func isGlobalIPPool(pool *lbv1.IPPool) bool {
+	return len(pool.Spec.Selector.Scope) == 1 && pool.Spec.Selector.Scope[0].Namespace == ipam.All &&
+		pool.Spec.Selector.Scope[0].Project == ipam.All && pool.Spec.Selector.Scope[0].GuestCluster == ipam.All
 }
