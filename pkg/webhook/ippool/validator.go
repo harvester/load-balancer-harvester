06615a85d9d7d778b723e475941ff62299fadd80
Jian Wang <jian.wang@suse.com>
2026-08-07 08:35:32 +0200

    Revert "Enhance global ippool selection with network matching"
    This reverts commit 5499454a8f09d50505778e3da23a9fa9150cf3b6.
    
    Related PR https://github.com/harvester/load-balancer-harvester/pull/104
    
    Signed-off-by: Jian Wang <jian.wang@suse.com>
    (cherry picked from commit e861dc948324084793b8af7dee22451d8a0d619c)

--- pkg/webhook/ippool/validator.go
+++ pkg/webhook/ippool/validator.go
@@ -13,6 +13,7 @@ import (
 	lbv1 "github.com/harvester/harvester-load-balancer/pkg/apis/loadbalancer.harvesterhci.io/v1beta1"
 	ctllbv1 "github.com/harvester/harvester-load-balancer/pkg/generated/controllers/loadbalancer.harvesterhci.io/v1beta1"
 	"github.com/harvester/harvester-load-balancer/pkg/ipam"
+	"github.com/harvester/harvester-load-balancer/pkg/utils"
 )
 
 type ipPoolValidator struct {
@@ -176,10 +177,10 @@ func checkAllocated(rs allocator.RangeSet, allocated map[string]string) error {
 }
 
 // checkSelector checks if the selector is valid.
-// It's allowed to create a global IP pool for each network only when there is no global IP pool for that network.
+// It's allowed to create a global IP pool only when there is no global IP pool.
 // When a pool checking scope overlaps with other pools, ignore the global IP pool.
 func (i *ipPoolValidator) checkSelector(pool *lbv1.IPPool) error {
-	if ipam.IsGlobalIPPool(pool) {
+	if isGlobalIPPool(pool) {
 		return i.checkGlobalIPPool(pool)
 	}
 
@@ -191,10 +192,9 @@ func (i *ipPoolValidator) checkSelector(pool *lbv1.IPPool) error {
 }
 
 func (i *ipPoolValidator) checkGlobalIPPool(pool *lbv1.IPPool) error {
-	// As a global IP pool is based on its network instead of the whole system now,
-	// some legacy global IP pools might not have the label.
-	// Just list all pools and iterate through them.
-	pools, err := i.ipPoolCache.List(labels.Everything())
+	pools, err := i.ipPoolCache.List(labels.Set(map[string]string{
+		utils.KeyGlobalIPPool: utils.ValueTrue,
+	}).AsSelector())
 	if err != nil {
 		return err
 	}
@@ -203,9 +203,7 @@ func (i *ipPoolValidator) checkGlobalIPPool(pool *lbv1.IPPool) error {
 		if p.Name == pool.Name {
 			continue
 		}
-		if ipam.IsGlobalIPPoolOfNetwork(p, pool.Spec.Selector.Network) {
-			return fmt.Errorf("there is already a global IP pool: %s for network %s", p.Name, pool.Spec.Selector.Network)
-		}
+		return fmt.Errorf("there is already a global IP pool: %s", p.Name)
 	}
 
 	return nil
@@ -242,12 +240,8 @@ func (i *ipPoolValidator) checkSelectorWithOthers(pool *lbv1.IPPool) error {
 	}
 
 	for _, p := range pools {
-		// not for same network
-		if p.Spec.Selector.Network != pool.Spec.Selector.Network {
-			continue
-		}
 		// skip itself and global IP pool
-		if p.Name == pool.Name || ipam.IsGlobalIPPool(p) {
+		if p.Name == pool.Name || isGlobalIPPool(p) {
 			continue
 		}
 		// priority could not be same if it's not zero
